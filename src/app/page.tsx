5f581a317e12b4b22f0e3a3e152babfdf51b1bfb
Sayan650 <sayanp650@gmail.com>
2024-02-25 19:18:27 +0530

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { Spotlight } from "@/components/ui/Spotlight";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { TracingBeam } from "@/components/ui/tracing-beam";
 import Image from "next/image";
+import { NavbarFloat } from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
     <>
+    <NavbarFloat/>
     <Hero />
     <AboutUs />
     </>

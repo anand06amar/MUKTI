7553453fc4ce735bb807691717f5ae110aa31dcd
Sayan650 <sayanp650@gmail.com>
2024-02-25 15:35:44 +0530

    components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     {/* <FloatingNav navItems={name}/> */}
-    <TracingBeam children />
-    
+    {/* <TracingBeam children /> */}
+
       hello mukti
-      <Spotlight/>
-      {/* <AboutUs /> */}
 
     </main>
   );

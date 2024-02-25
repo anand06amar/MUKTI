0f7ece58abb170f4ab3a5b16c407c57d2dbb2640
Sayan650 <sayanp650@gmail.com>
2024-02-25 15:31:17 +0530

    components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import AboutUs from "@/components/AboutUS";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    {/* <FloatingNav navItems={name}/> */}
+    <TracingBeam children />
       hello mukti
-      <AboutUs />
+      <Spotlight/>
+      {/* <AboutUs /> */}
+
     </main>
   );
 }

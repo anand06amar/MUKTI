580101f4619a487b54af648a47efc217a59d044d
GHOST0O7 <ssr.22u10038@btech.nitdgp.ac.in>
2024-02-25 20:00:35 +0530

    added counter component

--- src/components/countdown/counter.tsx
+++ src/components/countdown/counter.tsx
@@ -17,16 +17,19 @@ export default function Counter() {
 
     return (
       <section className="coming-soon-section">
-        <div className="auto-container">
-            <div className="bg-yellow-400 text-black font-bold rounded-xl ps-10 pe-10 md:flex ">
-                <div className="title-column items-center flex">
-                    <div className="inner-column hidden md:block none">
-                        <div className="text-2xl text-center font-bold text-slate-800 md:text-start ">Count Every<br></br>Second Until<br></br>the Event</div>
-                    </div>
+        <div className="auto-container ">
+            <div className="sm:w-[90vw] w-[400px] max-w-[900px] bg-yellow-400 text-black font-bold rounded-xl ps-10 pe-10 lg:flex">
+                <div className="title-column items-center flex justify-center">
+                    <div className="inner-column hidden lg:block none">
+                        <div className="text-2xl text-center font-bold text-slate-800 lg:text-start ">Count Every<br></br>Second Until<br></br>the Event</div>
+              </div>
+              <div className="inner-column pt-10 lg:hidden">
+                <div className="text-2xl text-center font-bold text-slate-800 lg:text-start ">Count Every Second Until the Event</div>
+              </div>
                 </div>
-                <div className="divider md:block md:flex-col hidden ps-8 pe-8 pt-3">
+                <div className="divider lg:block lg:flex-col hidden ps-8 pe-8 justify-center">
                     <div className="inner-column">
-                        <div className="text-center text-xl font-bold pt-2 text-slate-800 mt-5">|</div>
+                        <div className="text-center text-xl font-bold pt-2 text-slate-800 mt-8">|</div>
                         </div>
                         <div className="inner-column">
                         <div className="text-center text-xl font-bold pt-2 text-slate-800 mt-2">|</div>
@@ -38,14 +41,16 @@ export default function Counter() {
                 <div className="timer-column">
                     <div className="inner-column">
                             <div className="time-counter">
-                                <div className="time-countdown clearfix flex flex-row gap-10 m-4 pb-10 pt-10 ">
-                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-md"><span className="count font-bold text-black text-5xl ">00 </span>Days</div>
+                                <div className="time-countdown clearfix flex flex-wrap gap-10 m-4 pb-10 pt-10 justify-center ">
+                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-lg"><span className="count font-bold text-black text-5xl ">00 </span>Days</div>
                                     <div className="text-[#212639] text-4xl mt-1">:</div>
-                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-md"><span className="count font-bold text-black text-5xl ">00 </span>Hours</div>
+                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-lg"><span className="count font-bold text-black text-5xl ">00 </span>Hours</div>
+                    <div className="text-[#212639] text-4xl mt-1 hidden sm:block">:</div>
+                    
+                    
+                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-lg"><span className="count font-bold text-black text-5xl ">00 </span>Minutes</div>
                                     <div className="text-[#212639] text-4xl mt-1">:</div>
-                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-md"><span className="count font-bold text-black text-5xl ">00 </span>Minutes</div>
-                                    <div className="text-[#212639] text-4xl mt-1">:</div>
-                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-md"><span className="count font-bold text-black text-5xl ">00 </span>Seconds</div>                                    
+                                    <div className="counter-column flex flex-col gap-4 font-semibold justify-center items-center text-[#212639] text-lg"><span className="count font-bold text-black text-5xl ">00 </span>Seconds</div>                                    
                                 </div>
                             </div>
                     </div>
